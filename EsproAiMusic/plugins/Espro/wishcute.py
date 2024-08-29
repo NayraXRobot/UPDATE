c9040747c8ac054278cff59a677afc0b9ba9750b
𝙉𝙖𝙮𝙧𝙖 ✗ 𝙍𝙤𝙗𝙤𝙩 <168265371+NayraXRobot@users.noreply.github.com>
2024-08-29 14:00:59 +0530

    Update wishcute.py

--- EsproAiMusic/plugins/Espro/wishcute.py
+++ EsproAiMusic/plugins/Espro/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from EsproAiMusic import app 
 
-SUPPORT_CHAT = "EsproSupport"
+SUPPORT_CHAT = "ll_SUKOON_ZONE_GROUP_ll"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
